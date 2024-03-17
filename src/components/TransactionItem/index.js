cac0799297ffbd76ef574cdc8a0b4c7aa21287fb
Sallonii <121022481+Sallonii@users.noreply.github.com>
2024-03-17 19:22:19 +0530

    Update index.js

--- src/components/TransactionItem/index.js
+++ src/components/TransactionItem/index.js
@@ -1,1 +1,31 @@
-// Write your code here
+import './index.css'
+
+const TransactionItem = props => {
+  const {transactionDetails, deleteHistoryItem} = props
+  const {id, title, amount, displayText} = transactionDetails
+
+  const onClickingDelete = () => {
+    deleteHistoryItem(id)
+  }
+  return (
+    <div className="history-header">
+      <p className="history-values">{title}</p>
+      <p className="history-values">{amount}</p>
+      <p className="history-values">{displayText}</p>
+      <button
+        data-testid="delete"
+        type="button"
+        onClick={onClickingDelete}
+        className="delete-button"
+      >
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/money-manager/delete.png"
+          alt="delete"
+          className="delete-button-img"
+        />
+      </button>
+    </div>
+  )
+}
+
+export default TransactionItem
